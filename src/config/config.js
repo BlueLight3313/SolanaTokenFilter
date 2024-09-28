d997f226e9cb1e3d7adb5ec6507e7067fbd4a55f
BlueLight3313 <bluelight3303@outlook.com>
2024-09-29 00:23:27 +0300

    asfdasasfd

--- src/config/config.js
+++ src/config/config.js
@@ -2,9 +2,9 @@ export const MAIN_TITLE = "SOLANA TOKEN FILTER";
 
 export const HELIUS_API_KEY = "be5c2bf4-d358-4ff0-83fd-c1b4450a1c90";
 
-// export const NEW_TOKEN_URL = "https://solanacheck-07bdccc69c5e.herokuapp.com/getNewToken";
+export const NEW_TOKEN_URL = "https://solanacheck-07bdccc69c5e.herokuapp.com/getNewToken";
 // export const NEW_TOKEN_URL = "http://146.19.247.164:8818/getNewToken";
-export const NEW_TOKEN_URL = "http://94.131.101.170:8818/getNewToken";
+// export const NEW_TOKEN_URL = "http://94.131.101.170:8818/getNewToken";
 
 export const RAYDIUM_AUTHORITY_ADDRESS = "5Q544fKrFoe6tsEbD7S8EmxGTJYAKtTVhAW5Q5pge4j1";
 
